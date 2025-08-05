b9abe1a988fefe3abb412e615d36beb4a798339b
Rafael Ibanez <ribanez037@yahoo.com>
2025-08-05 16:16:07 -0600

    Versión de entrega para Challenge "Amigo Secreto"

--- app.js
+++ app.js
@@ -1,3 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let amigos = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    if (nombre === ""){
+        alert("No se ha ingresado nombre. Ingrese el nombre de un amigo");
+    }
+    else {
+        if (/\d/.test(nombre)){
+            alert("Los nombres no tienen números. Ingrese un nombre válido");
+            limpiarCaja();
+        }
+        else {
+            amigos.push(nombre);
+            console.log(amigos);
+            limpiarCaja();
+        }
+    }
+}
+
+function limpiarCaja(){
+    document.getElementById("amigo").value = "";
+}
+
+function sortearAmigo(){
+    let numeroAmigo = Math.floor(Math.random()*amigos.length);
+    let amigoSecreto = amigos[numeroAmigo]
+    if (amigos != ""){
+        alert(`El nombre de su amigo secreto es ${amigoSecreto}`);
+        condicionesIniciales()
+    }
+    else {
+        alert("Ingrese los nombres de sus amigos para poder comenzar");
+    }
+        
+}
+
+function condicionesIniciales(){
+    amigos = [];
+}
